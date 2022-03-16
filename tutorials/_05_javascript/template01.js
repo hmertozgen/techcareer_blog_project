dbba0b8d94c17c6db38ea29378c19d3f29fe8651
hamitmizrak <hamitmizrak@gmail.com>
2022-03-16 19:46:16 +0300

    console.log, alert,window,document,document.write

--- tutorials/_05_javascript/template01.js
+++ tutorials/_05_javascript/template01.js
@@ -4,3 +4,9 @@
 multiple comment
 */
 //alert("Merhabalar external js");
+////////////////////////////////////////////
+//alert("popup1");
+//window.alert("popup2");
+// console.log("loglama");
+// document: bu sayfa
+// document.write("Yazı yazdım");
