f6eb1ed0337fbb29d6a29268f20263b498f0ba24
hamitmizrak <hamitmizrak@gmail.com>
2022-03-16 20:06:55 +0300

    var,let,const

--- tutorials/_05_javascript/template01.js
+++ tutorials/_05_javascript/template01.js
@@ -30,15 +30,28 @@ multiple comment
 // x3=x1+x2;
 // console.log(x3)
 
-var x1,x2,x3;
-x1=44.16;
-x2=16;
-x3=   x1   +  x2;
-console.log(x3);
+// var x1,x2,x3;
+// x1=44.16;
+// x2=16;
+// x3=   x1   +  x2;
+// console.log(x3);
 
-var isLogin=true;
-console.log(isLogin)
+// var isLogin=true;
+// console.log(isLogin)
 
 //Number  ==> sayılar
 //string  ==> kelimeler
 //boolean ==> true false
+
+/////////////////////////////////////
+//dynamic type
+var global=66;
+global=11;
+console.log(global)
+
+let  local=44;    //ES6(2015)
+local=15;
+console.log(local)
+
+const sabit=3.14; //ES6(2015)
+console.log(sabit)
