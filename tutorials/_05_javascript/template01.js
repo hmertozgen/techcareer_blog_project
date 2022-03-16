c71e2c14c52baf013dd91b6ead68078865505b5d
hamitmizrak <hamitmizrak@gmail.com>
2022-03-16 19:58:15 +0300

    variables

--- tutorials/_05_javascript/template01.js
+++ tutorials/_05_javascript/template01.js
@@ -10,3 +10,35 @@ multiple comment
 // console.log("loglama");
 // document: bu sayfa
 // document.write("Yazı yazdım");
+
+/////////////////////////////////////////////
+//variables
+//camel case
+//sayı ile başlama
+//tire ile ayırma
+//$ ile başlayabilirsin
+// _ $ ile başlayabilirsin
+
+// var _person_Name44="Hamit";
+// console.log(_person_Name44);
+
+// var x1;
+// var x2;
+// var x3;
+// x1=44;
+// x2=16;
+// x3=x1+x2;
+// console.log(x3)
+
+var x1, x2,x3;
+x1=44.16;
+x2=16;
+x3=x1+x2;
+console.log(x3);
+
+var isLogin=true;
+console.log(isLogin)
+
+//Number  ==> sayılar
+//string  ==> kelimeler
+//boolean ==> true false
